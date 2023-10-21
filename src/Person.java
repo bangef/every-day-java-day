1716dd568ac018a6a1d2ceabb5a92f5c8e1895a9
bangef <ficrihnp@gmail.com>
2023-10-21 09:18:45 +0700

    learning: fields

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,5 @@
 public class Person { // this is class Person
+  String firstName; // deklaration varible with data type String
+  String lastName; // deklaration varible with data type String
+  final String Country = "Indonesia"; // key word final, like constant variables cannot be reassigned.
 }
