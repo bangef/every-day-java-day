bc38473ef36a84af657d0283b1673922999be075
bangef <ficrihnp@gmail.com>
2023-10-21 09:28:43 +0700

    learn: manipulation fields

--- src/App.java
+++ src/App.java
@@ -3,7 +3,9 @@ public class App {
         Person person = new Person(); // Create instance object from class Person.
         Person person2;
         person2 = new Person(); // Different way create instance object from class Person.
-
+        System.out.println(person.firstName); // output "null"
+        person.firstName = "Bangef"; // reassigned value 
+        System.out.println(person.firstName); // output Bangef
         System.out.println(person); // print unique id from memory 
         System.out.println(person2);
     }
