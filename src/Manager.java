37d613f1383cb474d7b47e59704188e6379d77db
bangef <ficrihnp@gmail.com>
2023-10-25 07:33:08 +0700

    learn: methode overriding

--- src/Manager.java
+++ src/Manager.java
@@ -1,11 +1,11 @@
-public class Manager { // Buat class Parent dengan nama Manager 
+public class Manager { // Buat class Parent dengan nama Manager
   String name; // field atau attribut String dengan nama variable name
 
-  Manager(String name){ // constructor class Manager denga params name
+  Manager(String name) { // constructor class Manager denga params name
     this.name = name; // assign ke attribut name dengan nilai dari constructor argumentnya
   }
 
-  void sayHello(String name){ // method sayHello dengan params String name
-    System.out.println("Hello " + name + ", my name is "+ this.name);
+  void sayHello(String name) { // method sayHello dengan params String name
+    System.out.println("Hello " + name + ", my name is " + this.name);
   }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,5 +1,11 @@
-public class VicePresident extends Manager { // class child VicePresident yang inheritance atau mewaris class parent Manager
-  VicePresident(String name){ // constructor
+public class VicePresident extends Manager { // class child VicePresident yang inheritance atau mewaris class parent
+                                             // Manager
+  VicePresident(String name) { // constructor
     super(name);
+  }
+
+  @Override
+  void sayHello(String name) { // method sayHello dengan params String name
+    System.out.println("Hello " + name + ", I am Vice President and my name is " + this.name);
   }
 }
